105d0379f90a9ecd9b119809dea574626557f432
Chris Logsdon <logsama@gmail.com>
2016-03-11 15:16:59 -0800

    updated favicon

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
  //app.use(methodOverride);
   //app.use(express.favicon());
   //app.use(favicon(__dirname + '/public/favicon.ico'));
