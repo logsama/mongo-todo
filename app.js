07a9ddae96ef2b0f8037355915fdbae453e720dd
Chris Logsdon <logsama@gmail.com>
2016-03-16 10:41:20 -0700

    updated favicon 5

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(favicon(__dirname + '/public/favicon.ico'));
  //app.use(methodOverride);
   //app.use(express.favicon());
